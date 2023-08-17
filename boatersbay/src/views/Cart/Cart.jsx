31b6db210248065b8bb6748a40cb3db862b72d5b
auglxw <augustineleexuanwei@gmail.com>
2023-08-17 22:03:55 +0800

    cart page

---
+++ boatersbay/src/views/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from '../../components/Heading/Heading';
+
+function Cart() {
+    return (
+        <div>
+            <Heading choice="cart" />
+            <p className="pageTitle">MY CART</p>
+        </div>
+    )
+}
+
+export default Cart;
